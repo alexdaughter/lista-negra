75c3188ca2df308148df96edd69b3cfc754aa29b
Alex Garcia <alejandragviana@gmail.com>
2019-01-23 19:15:42 +0100

    fetch is working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import {getPersons} from './services/personService';
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      results: []
+    }
+    this.getCriatutas();
+
+  }
+
+  getCriatutas(){
+    getPersons()
+      .then(data =>{
+        this.setState({
+          results: data.results
+        })
+      });
+  }
+
+  
+
   render() {
     return (
       <div className="App">
